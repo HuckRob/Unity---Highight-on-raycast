b224a6cd7de0cf921de9d9169b6050a6a28f5ae0
HuckRob <RobertBuentgen@u.boisestate.edu>
2023-02-27 14:55:31 -0700

    Added Selection Manager

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class SelectionManager : MonoBehaviour
 {
+    [SerializeField] private string selectableTag = "Selectable";
+    public Material highlightMaterial;
+    public Material defaultMaterial;
+
+    private Transform _selection;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,26 @@ public class SelectionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if(selectionRenderer != null)
+                {
+                selectionRenderer.material = highlightMaterial;
+                }
+            }
         
+        }
     }
 }
